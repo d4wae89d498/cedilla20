62815d47937935591284d4fcfd1c91c655dc8356
MarcFaussurier <faussurier.marc@icloud.com>
2022-08-06 17:07:15 +0200

    explicit macro init

--- match/test.c
+++ match/test.c
@@ -1,17 +1,19 @@
 #include "match.h"
 
-char *parentheses_macro(match_ctx *ctx)
+void macro_plvl_init(match_ctx *ctx)
+{
+    if (!exists(ctx->o, plvl))
+        rset(&(ctx->o), new (int, plvl, 0));
+}
+
+char *macro_plvl(match_ctx *ctx)
 {
     object_list **o = &(ctx->o);
-
     printf("pmacro called...\n");
-
-    if (!exists(*o, plvl))
-        rset(o, new (int, plvl, 0));
     if (match(ctx, str_is, "("))
         get(*o, plvl)->value += 1;
     else if (match(ctx, str_is, ")"))
-        get(*o, plvl)->value -= 1;    
+        get(*o, plvl)->value -= 1;
     else
         return 0;
     printf("parenthese found!\n");
@@ -21,8 +23,8 @@ char *parentheses_macro(match_ctx *ctx)
 
 int any(match_ctx *ctx, va_list ap)
 {
-    (void) ctx;
-    (void) ap;
+    (void)ctx;
+    (void)ap;
     return 1;
 }
 
@@ -40,13 +42,16 @@ int main()
     // TODO :: fix capture that only capture first char
 
     match_ctx ctx =
-        {
-            .o = 0,
-            .str = "  1 2  (4 + (9 * 8))",
-            .output = strdup("")};
-    set(&(ctx.o), macro_list, new (macro_function, m1, parentheses_macro));
+    {
+        .o = 0,
+        .str = "  1 2  (4 + (9 * 8))",
+        .output = strdup("")
+    };
+    macro_plvl_init(&ctx);
+    set(&(ctx.o), macro_list, new (macro_function, m1, macro_plvl));
     char *buffer = 0;
-    match(&ctx,
+    match
+    (&ctx,
           print_int, 10,
           skip, is_space, limit, 5, until, is_digit,
           skip, is_digit,
@@ -56,10 +61,10 @@ int main()
           skip, is_space,
           dump_str,
           parse, "(",
-        //  skip, any, until, parse, "(",
+          //  skip, any, until, parse, "(",
           dump_plvl,
           dump_str,
-          capture, &buffer,  until, parse, ")"
+          capture, &buffer, until, parse, ")"
     );
 
     printf("[capture=%s]\n", buffer);
